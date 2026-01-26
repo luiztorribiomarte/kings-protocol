3f8b42726ac5dea00064e8ab502a74809677ec75
luiztorribiomarte <luiztorribio@gmail.com>
2026-01-25 23:26:13 -0500

    Delete features/boot.js

--- features/boot.js
+++
@@ -1,7 +0,0 @@
-console.log("KP Boot loaded");
-
-// Safe init order
-window.addEventListener("load", () => {
-  if (typeof debugHabits === "function") debugHabits();
-  if (typeof renderLifeScore === "function") renderLifeScore();
-});
